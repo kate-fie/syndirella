5d3f1a3a97f024693956537ff3c9efc9fa0c1ba7
kate-fie <kate.k.fieseler@gmail.com>
2023-10-18 07:54:02 +0100

    🔧

--- job.sh
+++ job.sh
@@ -3,21 +3,21 @@
 # For scripts required see: https://gist.github.com/matteoferla/e0496d5766c12a0ae1738b943b41a536
 # A few things don't work in CentOS 7 due to GNU lib C (glibc) 2.17, so it has to be run in a Singularity container
 
-: << USAGE
-export JOB_SCRIPT=/data/xchem-fragalysis/shared/singularity.sh;
-export APPTAINER_CONTAINER=/data/xchem-fragalysis/shared/singularity/rockyplus.sif; # CHANGE?
-export JOB_INNER_SCRIPT=/data/xchem-fragalysis/kfieseler/repos/elaborate/job.sh; # CHANGE TO THIS PATH
-condor_submit /data/xchem-fragalysis/shared/target_script.condor
-USAGE
+#: << USAGE #########
+#export JOB_SCRIPT=/data/xchem-fragalysis/shared/singularity.sh;
+#export APPTAINER_CONTAINER=/data/xchem-fragalysis/shared/singularity/rockyplus.sif; # CHANGE?
+#export JOB_INNER_SCRIPT=/data/xchem-fragalysis/kfieseler/repos/elaborate/job.sh; # CHANGE TO THIS PATH
+#condor_submit /data/xchem-fragalysis/shared/target_script.condor
+#USAGE #########
 
-: << INSTALLATION
-export PIP_NO_USER=1; # gets ignored.
-export PYTHONUSERBASE=$CONDA_PREFIX; # double tap
-conda install -y -n base -c conda-forge openbabel plip;
-pip install -q fragmenstein
-pip install -q pyrosetta-help
-PYROSETTA_USERNAME=leventhal PYROSETTA_PASSWORD=paradox install_pyrosetta
-INSTALLATION
+#: << INSTALLATION #############
+#export PIP_NO_USER=1; # gets ignored.
+#export PYTHONUSERBASE=$CONDA_PREFIX; # double tap
+#conda install -y -n base -c conda-forge openbabel plip;
+#pip install -q fragmenstein
+#pip install -q pyrosetta-help
+#PYROSETTA_USERNAME=levinthal PYROSETTA_PASSWORD=paradox install_pyrosetta
+#INSTALLATION ############
 
 export HOST=${HOST:-$(hostname)}
 export USER=${USER:-$(users)}
@@ -26,7 +26,8 @@ source /etc/os-release;
 echo "Running script ${0} as $USER in $HOST which runs $PRETTY_NAME"
 # ---------------------------------------------------------------
 
-source /data/xchem-fragalysis/kfieseler/.bashrc;
+source /data/xchem-fragalysis/kfieseler/.bashrc
+conda activate fragmenstein
 
 cd /data/xchem-fragalysis/kfieseler/retrievesynthesizable
 
