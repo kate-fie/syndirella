36431fe5414f0cb8ec5bd899054cf72fe4e6a726
kate-fie <kate.k.fieseler@gmail.com>
2023-11-06 21:00:21 +0000

    🔧

--- utils/format_to_hippo.py
+++ utils/format_to_hippo.py
@@ -6,6 +6,7 @@ from typing import Dict, Any, Tuple
 import glob2
 import pandas as pd
 import shutil
+import ast
 
 def config_parser():
     parser = argparse.ArgumentParser()
@@ -15,36 +16,63 @@ def config_parser():
     return parser
 #-------------------------#
 
-def find_success_dirs(dir_path: str, elab: str, output: str) -> pd.DataFrame:
+def get_merged_data(root_path: str, dir_path: str, elab_suffix: str, output_suffix: str) -> pd.DataFrame:
     """
-    Find successful dirs from the merge of original elab csv and output csv from Fragmenstein.
-
-    Returns a list of dictionaries representing the rows in the merged CSV.
+    Merges original elab csv and output csv from Fragmenstein
 
     INPUT:
         dir_path: directory path of the base compound
-        elab: suffix of the elab csv file
-        output: suffix of the output csv file
+        elab_suffix: suffix of the elab csv file
+        output_suffix: suffix of the output csv file
     """
     merged_data = []
     elab_path = ''
     output_path = ''
-    csv_files = glob2.glob(dir_path)
+    full_path = os.path.join(root_path, dir_path) + '/*.csv'
+    csv_files = glob2.glob(full_path)
     for file in csv_files:
-        if elab in file:
+        if elab_suffix in file:
             elab_path = file
-        elif output in file:
+        elif output_suffix in file:
             output_path = file
     if elab_path == '' or output_path == '':
         return None
     elabdf = pd.read_csv(elab_path)
-    outputdf = pd.read_csv(output_path)
-    merged_data = elabdf.merge(outputdf, on='smiles', how='left', suffixes=('_elab', '_output'))
-    #Drop rows where there is no output or an error
-    success_data = merged_data.drop(merged_data[(merged_data['output'] != 'acceptable') | (merged_data['output'] != 'too moved'].index))
-    return success_data
+    # Check if elabdf is already merged by if it contains 'outcome' column
+    if 'outcome' in elabdf.columns:
+        acceptable_data = elabdf[elabdf['outcome'] == 'acceptable']
+    else:
+        outputdf = pd.read_csv(output_path, index_col=0)
+        merged_data = elabdf.merge(outputdf, on='smiles', how='left', suffixes=('_elab', '_output'))
+        #Save merged_data if not already created
+        merged_data.to_csv(elab_path, index=False)
+        #Drop rows where there is no output or an error
+        acceptable_data = merged_data[merged_data['outcome'] == 'acceptable']
+    return acceptable_data, elab_path
 
-def create_success_directories(root: str, dir_path: str, merged_data: pd.DataFrame) -> Tuple[Dict[str, int], int]:
+
+def move_folder(row, output_path, success_dir_path, df):
+    # Check if 'moved_to_success_dir' column exists in df, if not, add it
+    if 'moved_to_success_dir' not in df.columns:
+        df['moved_to_success_dir'] = False
+    # Define the source and destination paths
+    source = os.path.join(output_path, row['name'].replace('_', '-'))
+    destination = os.path.join(success_dir_path, row['name'].replace('_', '-'))
+    # Check if the destination directory already exists
+    if not os.path.exists(destination):
+        # If it does not exist, attempt to move the folder
+        try:
+            shutil.move(source, destination)
+            # Update the DataFrame if move is successful
+            df.at[row.name, 'moved_to_success_dir'] = True
+        except Exception as e:
+            # If there's an error, print it, but continue with the next iteration
+            print(f"Could not move {source} to {destination}: {e}")
+    else:
+        # If it exists, print a message and continue
+        print(f"Directory {destination} already exists, skipping.")
+
+def create_success_directories(root: str, dir_path: str, acceptable_data: pd.DataFrame, elab_path: str) -> Tuple[Dict[str, int], int]:
     """
     Create success directories and return a mapping of paths to number of files.
     """
@@ -52,11 +80,21 @@ def create_success_directories(root: str, dir_path: str, merged_data: pd.DataFra
     success_dir_path = os.path.join(root, dir_path, 'success')
     if not os.path.exists(success_dir_path):
         os.makedirs(success_dir_path)
+    output_path = os.path.join(root, dir_path) + '/output'
     # move dirs in success.csv to success dir
-    merged_data.apply(lambda row: shutil.move(os.path.join(root, dir_path, row['name']), success_dir_path), axis=1)
+    try:
+        acceptable_data.apply(lambda row: move_folder(row, output_path=output_path, success_dir_path=success_dir_path, df=acceptable_data), axis=1)
+    except Exception as e:
+        print(e)
     num_dirs = len(os.listdir(success_dir_path))
     success_dict[success_dir_path] = num_dirs
-    merged_data.to_csv(os.path.join(success_dir_path, 'success.csv'))
+    # Merge success.csv with elab.csv
+    elabdf = pd.read_csv(elab_path)
+    acceptable_data_subset = acceptable_data[['smiles', 'moved_to_success_dir']]
+    df = elabdf.merge(acceptable_data_subset, on='smiles', how='left', suffixes=('_elab', '_output'))
+    acceptable_data.to_csv(os.path.join(success_dir_path, 'success.csv'))
+    # Save merged data
+    df.to_csv(elab_path, index=False)
     return success_dict, num_dirs
 
 #----------------------------#
@@ -66,20 +104,27 @@ def main():
     total_success_dirs = []
     num_success_dirs = 0
     num_success_elabs = 0
-    for root, directory, files in os.walk(args.directory): # walk through home directory, where the directories of the elab compounds are
-        # Merge csvs of the original csv and output csv from Fragmenstein
-        success_data = find_success_dirs(directory, args.elab, args.output)
-        if success_data is None:
-            print(f"{directory} DOES NOT CONTAIN THE ELAB AND OUTPUT CSV FILES")
-            continue # go onto next directory of elab compounds
-        # Create success dirs
-        success_dict, num_dirs = create_success_directories(root, args.directory, merged_data)
-        num_success_dirs += 1
-        num_success_elabs += len(num_dirs)
-        # Store success dirs paths and number of successes within each
-        total_success_dirs.append(success_dict)
+    for root, directories, files in os.walk(args.directory): # walk through home directory, where the directories of the elab compounds are
+        for directory in directories:
+            if directory == 'success' or directory == 'output' or 'xtra_results' in directory:
+                continue
+            # Merge csvs of the original csv and output csv from Fragmenstein (if not already done)
+            try:
+                acceptable_data, elab_path = get_merged_data(root, directory, args.elab, args.output)
+                if acceptable_data is None:
+                    print(f"{directory} DOES NOT CONTAIN THE ELAB AND OUTPUT CSV FILES")
+                    continue # go onto next directory of elab compounds
+                # Create success dirs
+                success_dict, num_dirs = create_success_directories(root, directory, acceptable_data, elab_path)
+                num_success_dirs += 1
+                num_success_elabs += num_dirs
+                # Store success dirs paths and number of successes within each
+                total_success_dirs.append(success_dict)
+            except Exception as e:
+                print(f"Error in {directory}: {e}")
+                continue
     # Save results
-    with open('success_dirs.json', 'w') as f:
+    with open((os.path.join(args.directory, 'success_dirs.json')), 'w') as f:
         json.dump(total_success_dirs, f)
 
 if __name__ == "__main__":
