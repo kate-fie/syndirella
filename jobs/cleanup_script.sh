56fd79b88360a93d6b41d1b51e9917b99f4ee19c
kate-fie <kate.k.fieseler@gmail.com>
2023-11-20 10:52:02 +0100

    🔧 add cleanup_script.sh

--- jobs/cleanup_script.sh
+++ jobs/cleanup_script.sh
@@ -15,7 +15,7 @@
 
 #!/bin/bash
 
-home_dir_path="/data/xchem-fragalysis/kfieseler/D68EV3CPROA/elabs/1_step_1-4"
+home_dir_path="/data/xchem-fragalysis/kfieseler/D68EV3CPROA/elabs/2_step"
 suffixes_to_keep=( '.minimised.json' '.holo_minimised.pdb' '.minimised.mol' )
 
 # Function to check if a file has a suffix to keep
