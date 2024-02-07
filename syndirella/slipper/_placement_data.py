5a5767f0bf0443332c4beacac94d6348218b7a95
kate-fie <kate.k.fieseler@gmail.com>
2024-02-07 10:09:52 +0000

    😎 you can now get nice Fragmenstein output if it didnt' fully finish

---
+++ syndirella/slipper/_placement_data.py
@@ -0,0 +1,110 @@
+#!venv/bin/env python3
+"""
+syndirella.slipper._placement_data.py
+
+This module contains functions to get the placement data from a Fragmenstein run.
+"""
+import os
+import json
+import pandas as pd
+import csv
+import glob2
+
+def get_placement_data(products: pd.DataFrame, fragmenstein_output: str, library_output_dir: str) -> pd.DataFrame:
+    # Make fragmenstein_placements.csv
+    placements_path: str = make_fragmenstein_placements_csv(fragmenstein_output, library_output_dir)
+    # Read csv
+    placements: pd.DataFrame = pd.read_csv(placements_path)
+    # Merge products with success_df on 'name' column
+    merged_df = pd.merge(products, placements, on='name', how='left')
+    # drop index
+    if 'index' in merged_df.columns:
+        merged_df = merged_df.drop(columns=['index'])
+    # order merged_df by 'num_atom_difference' column
+    merged_df = merged_df.sort_values(by=['num_atom_diff'])
+    if 'Unnamed: 0' in merged_df.columns:
+        merged_df = merged_df.drop(columns=['Unnamed: 0'])
+    final_products_csv_path = find_products_csv(library_output_dir)
+    merged_output_path = final_products_csv_path.split('.csv')[0] + '_placements.csv'
+    merged_df.to_csv(merged_output_path, index=False)
+    return merged_df
+
+def find_products_csv(library_output_dir: str) -> str:
+    """
+    Given a directory, find the products and non-placements csv file.
+    """
+    # Pattern for matching files that contain 'products' but not '_placements' in their names
+    pattern = library_output_dir + '/*products*.csv'
+    matched_files = glob2.glob(pattern)
+    # Filtering out files that contain '_placements'
+    filtered_files = [file for file in matched_files if '_placements' not in file]
+    return filtered_files[0]
+
+def make_fragmenstein_placements_csv(output_path: str, library_output_dir: str) -> str:
+    """
+    This function makes a fragmenstein_placements.csv by looking through outputs.
+    """
+    # Make fragmenstein_placements.csv
+    headers = ['name', 'ΔΔG', 'ΔG_bound', 'ΔG_unbound', 'comRMSD']
+    collected_data = []
+    for subdir in os.listdir(output_path):
+        if subdir.startswith('.'):  # Skip hidden files/directories
+            continue
+        subdir_path = os.path.join(output_path, subdir)
+        if os.path.isdir(subdir_path):
+            for file in os.listdir(subdir_path):
+                if file.endswith('.json'):
+                    json_file_path = os.path.join(subdir_path, file)
+                    with open(json_file_path, 'r') as file:
+                        data = json.load(file)
+                        try:
+                            csv_row = make_success_csv_row(subdir, data)
+                            collected_data.append(csv_row)
+                        except Exception as e:
+                            print(f'Error: {e}')
+    csv_file_path = os.path.join(library_output_dir, 'fragmenstein_placements.csv')
+    if collected_data:
+        with open(csv_file_path, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(headers)
+            writer.writerows(collected_data)
+        return csv_file_path
+    return None
+
+def get_delta_delta_G(data: dict) -> float:
+    # Get the delta delta G value from the JSON file. Accounts for different formats.
+    try:
+        return data["Energy"]["xyz_∆∆G"]
+    except KeyError:
+        try:
+            bound = data["Energy"]["bound"]['total_score']
+            unbound = data["Energy"]["unbound"]['total_score']
+            ddG = bound - unbound
+            return ddG
+        except KeyError:
+            return float('inf')
+
+def get_bound_unbound(data: dict) -> tuple:
+    """Get the bound and unbound energy values from the JSON file. Accounts for different formats"""
+    try:
+        bound = data["Energy"]["xyz_bound"]
+        unbound = data["Energy"]["xyz_unbound"]
+        return bound, unbound
+    except KeyError:
+        try:
+            bound = data["Energy"]["bound"]["total_score"]
+            unbound = data["Energy"]["unbound"]["total_score"]
+            return bound, unbound
+        except KeyError:
+            return float('inf'), float('inf')
+
+def make_success_csv_row(subdir: str, data: dict) -> list:
+    """Make a row for the success.csv file."""
+    ddG = get_delta_delta_G(data)
+    bound, unbound = get_bound_unbound(data)
+    try:
+        rmsd = data["mRMSD"]
+    except KeyError:
+        rmsd = float('inf')
+    csv_row = [subdir, ddG, unbound, bound, rmsd]
+    return csv_row
--- syndirella/tests/test_all.py
+++ syndirella/tests/test_all.py
@@ -7,6 +7,9 @@ product of a reaction.
 """
 
 import unittest
+
+import pandas as pd
+
 from syndirella.cobblers_workshop._base import CobblersWorkshop
 from syndirella.cobblers_workshop._cobbler_bench import CobblerBench
 from ..smarts import SMARTSHandler
@@ -16,6 +19,112 @@ from rdkit import Chem
 from syndirella.slipper.slipper_synthesizer._base import SlipperSynthesizer
 from syndirella.slipper._base import Slipper
 import pickle
+
+class TestReactantsFiltering(unittest.TestCase):
+    def setUp(self):
+        self.reactants = [('OB(O)c1cccc2cc[nH]c12', 'Ic1cccc(I)n1'),
+                          ('Ic1cccc(-c2cccc3cc[nH]c23)n1', 'CCCB(O)O')]
+        self.product = 'CCCc1cccc(-c2cccc3cc[nH]c23)n1'
+        self.reaction_names = ['Sp2-sp2_Suzuki_coupling', 'Sp3-sp2_Suzuki_coupling']
+        self.num_steps = 2
+        self.output_dir = \
+            ('/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/tests/'
+             'reactants_filtering_w_labels')
+        self.filter = False
+
+        # need to set variables for Fragmenstein
+        self.template = '/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/tests/one_step_warren_A71EV2A/fragments/x0310_template.pdb'
+        self.hits = '/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/tests/one_step_warren_A71EV2A/fragments/clean_hits.sdf'
+        self.hits_names = ['x0566_0A']
+        self.n_cores = 8
+        self.timeout = 240
+        self.batch_num = 10000
+
+        self.first_step_atoms_ids_expansion: (
+            dict) = {0: None,
+                     1: None,
+                     2: True,
+                     3: True,
+                     4: True,
+                     5: None,
+                     6: None,
+                     7: False,
+                     8: False,
+                     9: None,
+                     10: None,
+                     11: None,
+                     12: None,
+                     13: True,
+                     14: None,
+                     15: True}
+
+        self.atom_ids_expansion: dict = {0: True,
+                                         1: True,
+                                         2: True,
+                                         3: None,
+                                         4: True,
+                                         5: True,
+                                         6: True,
+                                         7: None,
+                                         8: None,
+                                         9: False,
+                                         10: False,
+                                         11: None,
+                                         12: None,
+                                         13: None,
+                                         14: None,
+                                         15: True,
+                                         16: None,
+                                         17: True}
+
+    def test_get_products(self):
+        cobblers_workshop = CobblersWorkshop(self.product, self.reactants, self.reaction_names, self.num_steps,
+                                             self.output_dir, self.filter)
+        final_library = cobblers_workshop.get_final_library()
+        slipper = Slipper(final_library,
+                          atoms_ids_expansion=self.atom_ids_expansion,
+                          template=self.template,
+                          hits=self.hits,
+                          hits_names=self.hits_names,
+                          batch_num=self.batch_num)
+        self.products = slipper.get_products()
+        self.assertIsNotNone(self.products)
+        self.placements = slipper.place_products()
+        self.assertIsNotNone(self.placements)
+
+    def test_save(self):
+        cobblers_workshop = CobblersWorkshop(self.product, self.reactants, self.reaction_names, self.num_steps,
+                                             self.output_dir, self.filter)
+        final_library = cobblers_workshop.get_final_library()
+        final_library.save()
+
+    def test_load(self):
+        library = Library.load(self.output_dir)
+        self.assertIsNotNone(library)
+
+    def test_Delete(self):
+        final_library = Library.load(self.output_dir)
+        slipper = Slipper(final_library,
+                          atoms_ids_expansion=self.atom_ids_expansion,
+                          template=self.template,
+                          hits=self.hits,
+                          hits_names=self.hits_names,
+                          batch_num=self.batch_num)
+        slipper.output_path = '/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/tests/reactants_filtering_w_labels/output'
+        slipper.clean_up_placements()
+
+    def test_GetProductsDF(self):
+        final_library = Library.load(self.output_dir)
+        slipper = Slipper(final_library,
+                          atoms_ids_expansion=self.atom_ids_expansion,
+                          template=self.template,
+                          hits=self.hits,
+                          hits_names=self.hits_names,
+                          batch_num=self.batch_num)
+        slipper.products: pd.DataFrame = (pd.read_csv('/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/tests/reactants_filtering_w_labels/JFMKOYDGTWISRQ-UHFFFAOYSA-N_Sp3-sp2_Suzuki_coupling_products_2of2.csv'))
+        slipper.output_path = '/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/tests/reactants_filtering_w_labels/output'
+        placements: pd.DataFrame = slipper.get_placements_df()
+        self.assertIsNotNone(placements)
 
 class TestWarrenOneStep(unittest.TestCase):
     def setUp(self):
@@ -71,8 +180,7 @@ class TestWarrenOneStep(unittest.TestCase):
 
     def test_AtomIDExpansionLabeling(self):
         self.final_library = Library.load(self.output_dir)
-        self.slipper = Slipper(self.final_library, self.template, self.hits, self.hits_names, self.batch_num,
-                               False, self.atom_ids_expansion)
+        self.slipper = Slipper(self.final_library, self.template, self.hits, self.hits_names, self.batch_num, False)
         self.products = self.slipper.get_products()
         self.assertIsNotNone(self.products)
 
