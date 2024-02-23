7b2f2349086eb073f7bb69e0e8e285d65ef24a43
kate-fie <kate.k.fieseler@gmail.com>
2024-02-23 12:13:56 +0000

    changing paths...

--- syndirella/_cli_defaults.py
+++ syndirella/_cli_defaults.py
@@ -10,13 +10,13 @@ cli_default_settings = dict(
     ranking=os.environ.get('SYNDIRELLA_RANKING', 'num_atom_diff'),
     cutoff=float(os.environ.get('SYNDIRELLA_CUTOFF', 5.0)),
     rxn_smarts_path=os.environ.get('SYNDIRELLA_RXN_SMARTS_PATH',
-                                   "/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/"
+                                   "/data/xchem-fragalysis/kfieseler/syndirella/syndirella/"
                                    "constants/RXN_SMARTS_CONSTANTS.json"),
     reactant_filters_path=os.environ.get('SYNDIRELLA_REACTANT_FILTERS_PATH',
-                                         "/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/"
+                                         "/data/xchem-fragalysis/kfieseler/syndirella/syndirella/"
                                          "constants/REACTANT_FILTER_CONSTANTS_V2.json"),
     additional_rxn_options_path=os.environ.get('SYNDIRELLA_ADDITIONAL_RXN_OPTIONS_PATH',
-                                               "/Users/kate_fieseler/PycharmProjects/syndirella/syndirella/"
+                                               "/data/xchem-fragalysis/kfieseler/syndirella/syndirella/"
                                                "constants/ADDITIONAL_RXN_OPTIONS.json"),
     supressed_exceptions=(Exception,)
 )
