7b806b9c85b9f84b2aac3fed6d3ca80d9c54da08
kate-fie <kate.k.fieseler@gmail.com>
2024-04-05 11:25:15 +0100

    readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,14 @@ release = '2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+              'sphinx.ext.autodoc',
+              'sphinx.ext.doctest',
+              'sphinx.ext.extlinks',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +31,15 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+source_suffix = '.rst'
+master_doc = 'index'
+# project = 'ASE'
+# copyright = f'{datetime.date.today().year}, ASE-developers'
+# templates_path = ['templates']
+exclude_patterns = ['build']
+# default_role = 'math'
+# pygments_style = 'sphinx'
+autoclass_content = 'both'
