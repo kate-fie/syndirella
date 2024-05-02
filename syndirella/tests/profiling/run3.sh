fba8a353fbb2c57a25accfc1f4fe138fde450717
kate-fie <kate.k.fieseler@gmail.com>
2024-05-02 17:26:18 +0100

    profiling capability

---
+++ syndirella/tests/profiling/run3.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#SBATCH --job-name=profile
+#SBATCH --chdir=/opt/xchem-fragalysis-2/kfieseler
+#SBATCH --output=/opt/xchem-fragalysis-2/kfieseler/logs/slurm-error_%x_%j.log
+#SBATCH --error=/opt/xchem-fragalysis-2/kfieseler/logs/slurm-error_%x_%j.log
+# gpu partition is `gpu`
+#SBATCH --partition=main
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+##SBATCH --time=01:00:00
+
+# extras
+##SBATCH --nodes=1
+##SBATCH --exclusive
+##SBATCH --mem=<memory>
+##SBATCH --mem-per-cpu=<memory>
+##SBATCH --gres=gpu:1
+##SBATCH --constraint=<constraint>
+# this does nothing
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kate.fieseler@stats.ox.ac.uk
+
+# -------------------------------------------------------
+
+export SUBMITTER_HOST=$HOST
+export HOST=$( hostname )
+export USER=${USER:-$(users)}
+export HOME=$HOME
+source /etc/os-release;
+
+echo "Running $SLURM_JOB_NAME ($SLURM_JOB_ID) as $USER in $HOST which runs $PRETTY_NAME submitted from $SUBMITTER_HOST"
+echo "Request had cpus=$SLURM_JOB_CPUS_PER_NODE mem=$SLURM_MEM_PER_NODE tasks=$SLURM_NTASKS jobID=$SLURM_JOB_ID partition=$SLURM_JOB_PARTITION jobName=$SLURM_JOB_NAME"
+echo "Started at $SLURM_JOB_START_TIME"
+echo "job_pid=$SLURM_TASK_PID job_gid=$SLURM_JOB_GID topology_addr=$SLURM_TOPOLOGY_ADDR home=$HOME cwd=$PWD"
+
+# -------------------------------------------------------
+# CONDA
+source /opt/xchem-fragalysis-2/kfieseler/.bashrc
+# debug
+echo '$CONDA_PREFIX = ' $CONDA_PREFIX
+echo '$LD_LIBRARY_PATH = ' $LD_LIBRARY_PATH
+echo "which python = " `which python`
+# test conda
+echo -e "\nconda info: "
+conda activate syndirella
+conda info
+# -------------------------------------------------------
+
+cd $HOME2/syndirella/syndirella/tests/profiling
+
+pwd;
+export INPUT='base3.csv'
+export OUTPUT='profiling_output_base3.txt'
+export BASE_DIR='/opt/xchem-fragalysis-2/kfieseler/syndirella/syndirella/tests/profiling'
+
+echo "Running profile_pipeline.py";
+echo "INPUT: $INPUT"
+echo "OUTPUT: $OUTPUT"
+echo "BASE_DIR: $BASE_DIR"
+
+nice -19 python profile_pipeline.py \
+--input $INPUT \
+--output $OUTPUT \
+--base_dir $BASE_DIR;
+
+echo 'COMPLETE'
