0a1b703d052fb94a2220e2f4267ad963ad58c3c4
kate-fie <kate.k.fieseler@gmail.com>
2024-05-08 14:48:43 +0100

    trying sphinx-autoapi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../../"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -27,7 +31,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-autoapi_dirs = ['../../syndirella']
+autoapi_dirs = ['syndirella']
 
 
 
@@ -35,7 +39,7 @@ autoapi_dirs = ['../../syndirella']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 source_suffix = '.rst'
 master_doc = 'index'
